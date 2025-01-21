2893f261d0db0c6415f8b9e8e046d2958b3760c7
jsridha <jay.sridharan@gmail.com>
2025-01-21 09:16:29 -0500

    relocated images directory

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -9,7 +9,7 @@ export default function Dashboard() {
                 <div className="wd-dashboard-course">
                 <Link to="/Kambaz/Courses/1234/Home"
                         className="wd-dashboard-course-link" >
-                    <img src="../../images/reactjs.jpg" width={200} />
+                    <img src="/images/reactjs.jpg" width={200} />
                     <div>
                     <h5> CS1234 React JS </h5>
                     <p className="wd-dashboard-course-title">
@@ -22,7 +22,7 @@ export default function Dashboard() {
                 <div className="wd-dashboard-course">
                 <Link to="/Kambaz/Courses/1235/Home"
                         className="wd-dashboard-course-link" >
-                    <img src="../../images/angular.jpg" width={200} />
+                    <img src="/images/angular.jpg" width={200} />
                     <div>
                     <h5> CS1235 Angular JS </h5>
                     <p className="wd-dashboard-course-title">
@@ -35,7 +35,7 @@ export default function Dashboard() {
                 <div className="wd-dashboard-course">
                 <Link to="/Kambaz/Courses/1236/Home"
                         className="wd-dashboard-course-link" >
-                    <img src="../../images/network.jpg" width={200} />
+                    <img src="/images/network.jpg" width={200} />
                     <div>
                     <h5> CS1236 Networking </h5>
                     <p className="wd-dashboard-course-title">
@@ -48,7 +48,7 @@ export default function Dashboard() {
                 <div className="wd-dashboard-course">
                 <Link to="/Kambaz/Courses/1237/Home"
                         className="wd-dashboard-course-link" >
-                    <img src="../../images/data.jpg" width={200} />
+                    <img src="/images/data.jpg" width={200} />
                     <div>
                     <h5> CS1237 Data Mining </h5>
                     <p className="wd-dashboard-course-title">
@@ -61,7 +61,7 @@ export default function Dashboard() {
                 <div className="wd-dashboard-course">
                 <Link to="/Kambaz/Courses/1238/Home"
                         className="wd-dashboard-course-link" >
-                    <img src="../../images/algo.jpg" width={200} />
+                    <img src="/images/algo.jpg" width={200} />
                     <div>
                     <h5> CS1238 Algorithms </h5>
                     <p className="wd-dashboard-course-title">
@@ -74,7 +74,7 @@ export default function Dashboard() {
                 <div className="wd-dashboard-course">
                 <Link to="/Kambaz/Courses/1239/Home"
                         className="wd-dashboard-course-link" >
-                    <img src="./images/cloud.jpg" width={200} />
+                    <img src="/images/cloud.jpg" width={200} />
                     <div>
                     <h5> CS1239 Cloud Computer </h5>
                     <p className="../../wd-dashboard-course-title">
@@ -87,7 +87,7 @@ export default function Dashboard() {
                 <div className="wd-dashboard-course">
                 <Link to="/Kambaz/Courses/1240/Home"
                         className="wd-dashboard-course-link" >
-                    <img src="../../images/cap.jpg" width={200} />
+                    <img src="/images/cap.jpg" width={200} />
                     <div>
                     <h5> CS1240 Senior Capstone Project </h5>
                     <p className="wd-dashboard-course-title">
