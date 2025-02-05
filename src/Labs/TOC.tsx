7f567e8bcb2a0966babd6015f16d20f7e5a2f49c
jsridha <jay.sridharan@gmail.com>
2025-02-04 23:16:17 -0500

    added name and github link

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -19,7 +19,10 @@ export default function TOC() {
                 <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="https://github.com/jsridha/kambaz-react-web-app">
+                <Nav.Link
+                    id="wd-github"
+                    href="https://github.com/jsridha/kambaz-react-web-app"
+                >
                     My GitHub
                 </Nav.Link>
             </Nav.Item>
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,15 +6,17 @@ import Lab3 from "./Lab3";
 import TOC from "./TOC";
 
 export default function Labs() {
-  return (
-    <div>
-      <h1>Labs</h1>
-      <TOC />
-      <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
-        <Route path="Lab1" element={<Lab1 />} />
-        <Route path="Lab2/*" element={<Lab2 />} />
-        <Route path="Lab3" element={<Lab3 />} />
-      </Routes>
-    </div>
-);}
+    return (
+        <div>
+            <h1>Labs</h1>
+            <h2>Jay Sridharan</h2>
+            <TOC />
+            <Routes>
+                <Route path="/" element={<Navigate to="Lab1" />} />
+                <Route path="Lab1" element={<Lab1 />} />
+                <Route path="Lab2/*" element={<Lab2 />} />
+                <Route path="Lab3" element={<Lab3 />} />
+            </Routes>
+        </div>
+    );
+}
