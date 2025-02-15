92aef1cee2afe77e1e34680e3ed16cbe0020a5e2
jsridha <jay.sridharan@gmail.com>
2025-02-15 15:06:31 -0500

    Through 3.1.6

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,22 @@
+import BooleanVariables from "./BooleanVariables";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInLine";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+
 export default function Lab3() {
     return (
         <div>
             <h2>Lab 3</h2>
+            <VariablesAndConstants />
+            <VariableTypes />
+            <BooleanVariables />
+            <IfElse />
+            <TernaryOperator />
+            <ConditionalOutputIfElse />
+            <ConditionalOutputInline />
         </div>
     );
 }
