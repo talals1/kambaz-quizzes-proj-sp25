902eddad3c9a2839b07ad5778b90ed2c1aa32309
jsridha <jay.sridharan@gmail.com>
2025-02-15 17:24:03 -0500

    through 3.6.2

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -32,6 +32,7 @@ import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
 
 export default function Lab3() {
+    console.log("Hello World!");
     return (
         <div id="wd-lab3">
             <h2>Lab 3</h2>
