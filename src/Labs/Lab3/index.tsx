1e1561b10e0ca058d89255a145a0a6c9c12ae418
jsridha <jay.sridharan@gmail.com>
2025-02-15 17:17:39 -0500

    through 3.5.3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,3 +1,4 @@
+import Add from "./Add";
 import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
 import ArrayIndexAndLength from "./ArrayIndexAndLength";
 import ArrowFunctions from "./ArrowFunctions";
@@ -12,14 +13,17 @@ import FindFunction from "./FindFunction";
 import FindIndex from "./FindIndex";
 import ForLoops from "./ForLoops";
 import FunctionDestructing from "./FunctionDestructing";
+import Highlight from "./Highlight";
 import House from "./House";
 import IfElse from "./IfElse";
 import ImpliedReturn from "./ImpliedReturn";
 import JsonStringify from "./JSONStringify";
 import LegacyFunctions from "./LegacyFunctions";
 import MapFunction from "./MapFunction";
+import PathParameters from "./PathParameters";
 import SimpleArrays from "./SimpleArrays";
 import Spreading from "./Spreading";
+import Square from "./Square";
 import Styles from "./Styles";
 import TemplateLiterals from "./TemplateLiterals";
 import TernaryOperator from "./TernaryOperator";
@@ -29,7 +33,7 @@ import VariableTypes from "./VariableTypes";
 
 export default function Lab3() {
     return (
-        <div>
+        <div id="wd-lab3">
             <h2>Lab 3</h2>
             <VariablesAndConstants />
             <VariableTypes />
@@ -62,6 +66,19 @@ export default function Lab3() {
 
             <Classes />
             <Styles />
+
+            <Add a={3} b={4} />
+            <h4>Square of 4</h4>
+            <Square>4</Square>
+            <hr />
+            <Highlight>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Suscipitratione eaque illo minus cum, saepe totam vel nihil
+                repellat nemo explicabo excepturi consectetur. Modi omnis minus
+                sequi maiores, provident voluptates.
+            </Highlight>
+
+            <PathParameters />
         </div>
     );
 }
