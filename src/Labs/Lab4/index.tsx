ea46c6c328e6105e4ca2b2d93d3a57e57894e5d8
jsridha <jay.sridharan@gmail.com>
2025-03-03 19:08:05 -0500

    through 4.3.5.2

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -8,6 +8,7 @@ import ObjectStateVariable from "./ObjectStateVariables";
 import ParentStateComponent from "./ParentStateComponent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import ReduxExamples from "./ReduxExamples";
 import StringStateVariables from "./StringStateVariables";
 
 export default function Lab4() {
@@ -29,6 +30,7 @@ export default function Lab4() {
             <ObjectStateVariable />
             <ArrayStateVariable />
             <ParentStateComponent />
+            <ReduxExamples />
         </div>
     );
 }
