7a1ddc0236866c323d959e864a02d8d1be7c1465
Stone Holt <stoneholt22@gmail.com>
2025-04-12 18:25:43 -0400

    add actual dropdown buttons to quiz controls

--- src/Kambaz/Courses/Quizzes/QuizControlButtons.tsx
+++ src/Kambaz/Courses/Quizzes/QuizControlButtons.tsx
@@ -1,12 +1,26 @@
 import { IoBan, IoEllipsisVertical } from "react-icons/io5";
 import GreenCheckmark from "../Modules/GreenCheckmark";
+import { useNavigate } from "react-router";
+import { Dropdown, DropdownButton } from "react-bootstrap";
 
 export default function QuizControlButtons({ quiz } : { quiz: any}) {
+    const navigate = useNavigate();
     return (
-        <div>
+        <div className="float-end">
             {quiz.published === "YES" ? <GreenCheckmark /> :
             <IoBan style={{ color: "red" }}/> }
-            <IoEllipsisVertical className="fs-4" />
+            <DropdownButton variant="secondary" title={<IoEllipsisVertical className="fs-4" />} onClick={(e) => e.preventDefault()} >
+                <Dropdown.Item onClick={(e) => {
+                        e.preventDefault();
+                        navigate(`${quiz._id}`);
+                    }}> Edit Quiz </Dropdown.Item>
+                <Dropdown.Item>
+                    Delete Quiz
+                </Dropdown.Item>
+                <Dropdown.Item>
+                    Publish Quiz
+                </Dropdown.Item>
+            </DropdownButton>
         </div>
     );
 }
