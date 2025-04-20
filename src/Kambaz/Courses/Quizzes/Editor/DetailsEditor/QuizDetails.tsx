4f0c5ae6bada28cf9a60d8fab9ecc0c9878782f0
Talal Siddiqui <talal.siddiqui11@gmail.com>
2025-04-20 11:58:21 -0400

    Add preview button

--- src/Kambaz/Courses/Quizzes/Editor/DetailsEditor/QuizDetails.tsx
+++ src/Kambaz/Courses/Quizzes/Editor/DetailsEditor/QuizDetails.tsx
@@ -2,20 +2,31 @@ import { useState } from "react";
 import { Button } from "react-bootstrap";
 import QuizDetailsEditor from "./QuizDetailsEditor";
 import QuizDetailsViewer from "./QuizDetailsViewer";
+import { useParams } from "react-router-dom";
 
 export default function QuizDetails() {
+    const { cid, qid } = useParams();
     const [useEditor, setUseEditor] = useState(false);
 
     return (
         <div>
             {!useEditor && (
-                <Button
-                    variant="danger"
-                    className="float-end"
-                    onClick={() => setUseEditor(!useEditor)}
-                >
-                    Edit Details
-                </Button>
+                <>
+                    <Button
+                        variant="danger"
+                        className="float-end"
+                        href={`#/Kambaz/Courses/${cid}/Quizzes/${qid}/take_quiz`}
+                    >
+                        Preview the Quiz
+                    </Button>
+                    <Button
+                        variant="danger"
+                        className="float-end"
+                        onClick={() => setUseEditor(!useEditor)}
+                    >
+                        Edit Details
+                    </Button>
+                </>
             )}
             <br />
             {useEditor ? (
